9a6c9e97a47b69d5c55f7eba6faf7cb01e762604
danny <dantonzhao@gmail.com>
2017-05-18 23:22:31 -0500

    right and left split complete, need to determine how speed controllers deal with the pwm forward/reverse

--- src/ardu_out/firmware/movement.cpp
+++ src/ardu_out/firmware/movement.cpp
@@ -7,6 +7,14 @@ ros::NodeHandle nh;
 
 geometry_msgs::Twist twst_msg;
 ros::Publisher chatter("twist", &twst_msg);
+int frontL = 3;
+int frontR = 9;
+int rearL = 10;
+int rearR = 11;
+double left_percent;
+double right_percent;
+double left_speed;
+double right_speed;
 
 void messageMV(const geometry_msgs::Twist& move_msg)
 {
@@ -14,11 +22,18 @@ void messageMV(const geometry_msgs::Twist& move_msg)
   double turn = move_msg.angular.z; //arduino hardware (UNO and ATMEGA based)
   twst_msg.linear.x = forward;
   twst_msg.angular.z = turn;
+  //angular determines percent devoted to each side
+  left_percent = (turn>0)?(0.5*(1 + turn/10)):(0.5*(1 + turn/10));
+  right_percent = 1-left_percent;
+  left_speed = (int)(left_percent*forward*255/10);
+  right_speed = (int)(right_percent*forward*255/10);
+  analogWrite(frontL,left_speed);
+  analogWrite(frontR,right_speed);
+  analogWrite(rearL,left_speed);
+  analogWrite(rearR,right_speed);
 }
 
-ros::Subscriber<geometry_msgs::Twist> input("turtle1/cmd_vel", &messageMV); 
-
-char hello[13] = "hello world!";
+ros::Subscriber<geometry_msgs::Twist> input("turtle1/cmd_vel", &messageMV);
 
 void setup()
 {
@@ -31,5 +46,5 @@ void loop()
 {
   chatter.publish( &twst_msg );
   nh.spinOnce();
-  delay(1);
+  delay(5);
 }
